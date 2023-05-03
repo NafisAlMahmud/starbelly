18b918c6e22153f45ee846aa89b34102ab14aff3
Project_3 <anik2115@gmail.com>
2023-05-04 02:14:57 +0600

    navbar active route set

--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Blogs from "../Blogs/Blogs";
 import { AuthContext } from "../../provider/AuthProvider";
 
@@ -14,29 +14,43 @@ const Header = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <Link to="/">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "default")}
+        >
           {" "}
-          <a className="btn btn-ghost normal-case text-4xl font-extrabold">
+          <a className="btn btn-ghost normal-case text-4xl font-extrabold hover:text-white ">
             Star<span className="text-slate-500">belly</span>
           </a>
-        </Link>
+        </NavLink>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          <li className="text-xl font-semibold">
-            <Link to="blogs">Blogs</Link>
+          <li className="text-xl font-semibold ">
+            <NavLink
+              to="blogs"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              Blogs
+            </NavLink>
           </li>
           <li>
-            <Link to="login">
-              <button className="px-4 py-2 bg-slate-500 text-white rounded">
+            <NavLink
+              to="login"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              <button className="btn bg-slate-500 text-white rounded">
                 Login
               </button>
-            </Link>
-            <Link to="signup">
-              <button className="px-4 py-2 bg-slate-900 text-white rounded">
+            </NavLink>
+            <NavLink
+              to="signup"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              <button className="btn bg-slate-900 text-white rounded">
                 Sign Up
               </button>
-            </Link>
+            </NavLink>
             {user && (
               <span>
                 Welcome {user.email}{" "}
